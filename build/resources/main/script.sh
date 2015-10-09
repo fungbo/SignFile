a1c7c9e859a277903430c13f1be2624a6788931d
bfeng <bfeng@thoughtworks.com>
2015-10-09 10:47:04 +0800

    remove useless files

--- build/resources/main/script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-eval "openssl pkcs8 -topk8 -outform DER -in $1/key.pem -inform PEM -out $1/key.pk8 -nocrypt"
-eval "java -jar signapk.jar certificate.pem $1/key.pk8 $2 $1/$2.signed.jar"
